05c44d5fa25be56fe6414bf1f9b065598eb037ed
marvazy <marva94@gmail.com>
2023-09-07 21:39:39 +0300

    fix script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,5 @@
 # install poetry and the dev-dependencies of the project
 python -m pip install --upgrade setuptools
 curl -sSL https://install.python-poetry.org | python3 - --version 1.5.1
-python -m poetry update --lock
-python -m poetry install
+poetry update --lock
+poetry install
